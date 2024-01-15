7d0ee439a6f43b760a1a2d860a4a6bce5b20498c
rivka <@DESKTOP-783IDMP>
2024-01-15 23:11:13 +0200

    #

--- ArtBL/Modelsa/ArtProjectContext.cs
+++ ArtBL/Modelsa/ArtProjectContext.cs
@@ -28,7 +28,7 @@ namespace ArtDL.Modelsa
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-783IDMP\\SQLEXPRESS;Database=ArtProject;Trusted_Connection=True;encrypt=false;");
+                optionsBuilder.UseSqlServer("Server=DESKTOP-783IDMP\\SQLEXPRESS;Database=ArtProject;Trusted_Connection=True");
             }
         }
 
@@ -125,6 +125,10 @@ namespace ArtDL.Modelsa
                 entity.Property(e => e.Password).HasMaxLength(50);
 
                 entity.Property(e => e.Phone).HasMaxLength(50);
+
+                entity.Property(e => e.ProfilImgPath)
+                    .HasMaxLength(50)
+                    .HasColumnName("profilImgPath");
 
                 entity.HasOne(d => d.Level)
                     .WithMany(p => p.Users)
